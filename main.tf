551ea1240a080a28ae2bf772630eae18b91f7411
Sai13041997 <praneethsattenapalli1310@gmail.com>
2026-06-18 20:06:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -230,12 +230,20 @@ resource "aws_api_gateway_deployment" "this" {
     aws_api_gateway_method.this,
     aws_api_gateway_authorizer.jwt,
   ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_api_gateway_stage" "this" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
   deployment_id = aws_api_gateway_deployment.this.id
   stage_name    = var.stage_name
+
+  depends_on = [
+    aws_api_gateway_deployment.this,
+  ]
 }
 
 # ----------------------------
